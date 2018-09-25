dc81d4c8c9badf069834fc3e945ad1759abed062
Narayana Wijaya <narayana.wijaya@geekseat.com.au>
2018-09-26 01:48:39 +0700

    Initial Commit

---
+++ Mini Project - Search Filter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mini Project - Search Filter
+//
+//  Created by Narayana Wijaya on 26/09/18.
+//  Copyright © 2018 Narayana Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
